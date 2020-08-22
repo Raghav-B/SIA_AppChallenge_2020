5630bd014223a2cb3dd8d566dad46f535aaf4241
Raghav-B <raghavbhardwaj1998@gmail.com>
2020-08-22 23:54:03 +0800

    Add krisshop

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { withRouter, Link } from 'react-router-dom'
+import { withRouter, Link, BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import "./Home.css";
 
 import ProgressBar from "./ProgressBar.js";
+import KrisShop from "./KrisShop.js";
 
 function Home() {
     return (
@@ -11,7 +12,19 @@ function Home() {
             <img src={require("./imgs/sia_full_logo.png")} className="sia-full-logo"/>
             <div className="home-ellipse"/>
 
-            <ProgressBar></ProgressBar>
+            <div className="home-content">
+                <ProgressBar flightNumber={"SQ213"} timeLeft={"3 hrs 15 mins"}></ProgressBar>
+
+                <Switch>
+                    <Route path="/home" exact component={() => (<KrisShop/>)}/>
+                    <Route path="/home/lounge" exact component={() => (<div></div>)}/>
+                    <Route path="/home/checklist" exact component={() => (<div></div>)}/>
+                    <Route path="/home/chatbot" exact component={() => (<div></div>)}/>
+                    <Route path="/home/chatbot" exact component={() => (<div></div>)}/>
+                </Switch>
+            </div>
+            
+            
 
 
         </div>
--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -5,8 +5,64 @@ import "./ProgressBar.css";
 
 function ProgressBar(props) {
     return (
-        <div>
-            AHHAJHAJAHJh
+        <div className="progress-bar-container">
+            <span className="welcome-text">Welcome Mr Ojus Sharma,</span>
+            <br/>
+            <span className="flight-text">Your flight <b>{props.flightNumber}</b> departs in {props.timeLeft}</span>
+            <br/>
+
+            <div className="actual-bar">
+                <div className="full-bar"></div>
+                <div className="elapsed-bar"></div>
+                
+                <div className="indicator-line" style={
+                        {"left": "50px",
+                        "top": "170px"}}>
+                </div>
+                <div className="indicator-text" style={
+                    {"left": "10px",
+                    "top": "198px"}}>
+                    Check-In opens<br/>
+                    1000
+                </div>
+
+                <div className="indicator-line" style={
+                        {"left": "270px",
+                        "top": "170px"}}>
+                </div>
+                <div className="indicator-text" style={
+                    {"left": "240px",
+                    "top": "198px"}}>
+                    Gate opens<br/>
+                    1400
+                </div>
+                
+                <div className="indicator-line" style={
+                        {"left": "340px",
+                        "top": "150px"}}>
+                </div>
+                <div className="indicator-text" style={
+                    {"left": "313px",
+                    "top": "119px"}}>
+                    Departs at<br/>
+                    1600
+                </div>
+                
+                <div className="time-left-text" style={
+                    {"left": "215px",
+                    "top": "167px"}}>
+                    1h 15m
+                </div>
+
+                <div className="time-left-text" style={
+                    {"left": "310px",
+                    "top": "167px"}}>
+                    2h
+                </div>
+                
+            </div>
+
+            <hr/>
         </div>
     );
 }
